ea99533b78371c374858f857ddeb6558dade01d7
Cory Alder <coryalder@gmail.com>
2016-05-05 01:43:29 -0700

    First commit

---
+++ Foodtruck/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  FoodTruck
+//
+//  Copyright © 2016 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FoodTruck.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        FoodTruck *truckA = [[FoodTruck alloc] initWithName:@"Take a Bao" andFoodType:@"bao"];
+        
+        FoodTruck *truckB = [[FoodTruck alloc] initWithName:@"Tim Shortons" andFoodType:@"shortbread"];
+        
+        // create instances of your delegate class
+        // set truckA and truckB's delegate to your new instance.
+        
+        [truckA serve:10];
+        [truckB serve:5];
+        
+        [truckA cashOut];
+        [truckB cashOut];
+    }
+    return 0;
+}
